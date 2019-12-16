f650225938bc0d07be9eb5d3e6ee515b79518f50
prudnikovv <prudnikovv@ukr.net>
2019-12-16 06:25:49 +0000

    user.downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   validates :email, :username, uniqueness:true
   validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, length: { maximum: 255 }
   validates :username, format: /\A[a-zA-Z0-9_]+\z/, length: { maximum: 40 }
+
+  before_validation :before_validation
+
+  def before_validation
+    username.downcase!
+  end
 end
