f5db6d686d922a8e60c042dc4f0471f8800df5e3
PrudnikovV <Prudnikovv@ukr.net>
2019-12-16 10:06:50 +0200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :email, :username, presence:true
   validates :email, :username, uniqueness:true
-  validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, length: { maximum: 255 }
+  validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_]+/.[a-zA-Z0-9\-_.]+\z/, length: { maximum: 255 }
   validates :username, format: /\A[a-zA-Z0-9_]+\z/, length: { maximum: 40 }
 
   before_validation :before_validation
